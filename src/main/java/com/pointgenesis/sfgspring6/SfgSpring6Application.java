744d99a045ea3bdccab55b04d1c51fa3afaeb1f9
Travis Steinmetz <travis.steinmetz@yahoo.com>
2023-09-25 23:20:17 -0400

    initial commit

---
+++ src/main/java/com/pointgenesis/sfgspring6/SfgSpring6Application.java
@@ -0,0 +1,13 @@
+package com.pointgenesis.sfgspring6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgSpring6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgSpring6Application.class, args);
+    }
+
+}
